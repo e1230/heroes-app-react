ba87e2ea7a71014824709925a100ffa3707f2460
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-21 16:46:44 -0500

    diseño pantalla de búsqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,36 @@
+import { HeroCard } from "../components/HeroCard";
+
 export const SearchPage = () => {
   return (
     <>
       <h1>Search Page</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          Searching
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="select a Hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+          <div className="alert alert-primary">Search a Hero</div>
+          <div className="alert alert-danger">
+            No results for <b>ABC</b>
+          </div>
+          {/* {<HeroCard />} */}
+        </div>
+      </div>
     </>
   );
 };
