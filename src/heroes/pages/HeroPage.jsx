da1122ccd74ce2c2d63430644854dbf7a97a1cbf
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-21 13:59:04 -0500

    leer argumento por url

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,13 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers/getHeroById";
+
 export const HeroPage = () => {
+  const { id } = useParams();
+  const hero = getHeroById(id);
+  if (!hero) return <Navigate to="/marvel" />;
   return (
     <>
-      <h1>Hero Page</h1>
+      <h1>{hero.superhero}</h1>
     </>
   );
 };
