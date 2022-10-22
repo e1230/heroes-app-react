b4ebb4c77ada9236f5fb43664e9cd1464df5d60b
e1230 <embeltrans@correo.udistrital.edu.co>
2022-10-22 13:58:34 -0500

    mantener usuario loggeado en LocalStorage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -3,21 +3,24 @@ import { typesAuth } from "../types/types.auth";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 
-const initialState = {
-  logged: false,
-};
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
 
+  return {
+    logged: !!user,
+    user: user,
+  };
+};
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = "") => {
+    const user = { id: "ABC", name: name };
     const action = {
       type: typesAuth.login,
-      payload: {
-        id: "ABC",
-        name: name,
-      },
+      payload: user,
     };
+    localStorage.setItem("user", JSON.stringify(user));
     dispatch(action);
   };
 
